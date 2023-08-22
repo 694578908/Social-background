bfd764cc39683de91aa6a8ee468c60a02a54f4d4
fu.yang <1003107864@qq.com>
2023-08-22 18:34:41 +0800

    请求pre登录接口

---
+++ test_get_request.py
@@ -0,0 +1,13 @@
+import requests
+import pytest
+def test_case_login():
+    print("Test case is running!")
+    url = "http://higher8pre.douxiangapp.com/dboxAdmin/v1/security/manageLogin/"
+    data = {"userName": "1123132", "pwd": "112321231"}
+    response = requests.post(url=url, json=data)
+    res = response.json()
+    print("Response JSON:", res)
+    assert response.status_code == 200
+if __name__ == "__main__":
+    pytest.main()
+
