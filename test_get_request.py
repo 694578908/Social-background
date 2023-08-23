12c43aaab4d287bed9a8d416bdd39e2baa2468c8
fu.yang <1003107864@qq.com>
2023-08-23 09:35:31 +0800

    请求pre登录接口

--- test_get_request.py
+++ test_get_request.py
@@ -1,10 +1,15 @@
 import requests
 import pytest
-def test_case_login():
-    url = "http://higher8pre.douxiangapp.com/dboxAdmin/v1/security/manageLogin/"
-    data = {"userName": "1123132", "pwd": "112321231"}
-    response = requests.post(url=url, json=data)
-    print(response.json())
-if __name__ == "__main__":
-    pytest.main()
+
+class TestRequest:
+
+    def test_case_login(self):
+        url = "http://higher8pre.douxiangapp.com/dboxAdmin/v1/security/manageLogin/"
+        data = {"userName": "1123132", "pwd": "112321231"}
+        response = requests.post(url=url, json=data)
+        print(response.json())
+
+        assert response.status_code == 200
+    if __name__ == "__main__":
+        pytest.main(['-vs'])
 
