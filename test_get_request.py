90020b7ada98f059c5fc95b603d16b698bcfe467
fu.yang <1003107864@qq.com>
2023-08-28 10:54:23 +0800

    添加冒烟装饰器，用例运行配置

--- test_get_request.py
+++ test_get_request.py
@@ -2,19 +2,23 @@ import requests
 import pytest
 
 class TestRequest:
-    #全局变量
+    # 全局变量
     token = ''
+    @pytest.markers.smoke
+    # 账号登录
     def testcase_login(self):
         url = "http://higher8pre.douxiangapp.com/dboxAdmin/v1/security/manageLogin"
         data = {"userName": "zqj", "pwd": "123456"}
-        response = requests.post(url=url, json=data)
+        response = requests.request("post", url=url, json=data)
         print(response.json())
         TestRequest.token = response.json()['data']
         assert response.status_code == 0
 
+    @pytest.markers.smoke
+    # 创建藏品
     def testcase_collection(self):
         url = "http://higher8pre.douxiangapp.com/dboxAdmin/v1/nft"
-        data = {"supplementaryType":1,"mainImageDisplay":0,"nftType":3,"isOnline":1,
+        data = {"supplementaryType": 1,"mainImageDisplay":0,"nftType":3,"isOnline":1,
         "canBuy":1,"numRandom":1,"openSend":"true","sendCloseTime":0,"sort":0,"title":"自动化测试",
         "description":"123","mainImg":"https://dboximg.douxiangapp.com/img/nft/1692756890755.jpg",
         "displayImgs":["https://dboximg.douxiangapp.com/img/nft/1692756926707.png"],
@@ -24,7 +28,7 @@ class TestRequest:
         "publishName":"龙之岛DOD","detailImgs":["https://dboximg.douxiangapp.com/video/20230823/20230823101529_butf.png"],
         "tags":["12"],"price":0.01,"firstStock":"","stock":10,"limitStock":12,"publishTime":"2023-08-23 10:16:34",
         "categories":[[{"categoryId":"707","level":1,"name":"其他"},{"categoryId":"708","level":2,"name":"其他"}]],
-        "openSell":"true,"sellCloseDay":0,"isPriorityBuy":0,"priorityDate":"","priorityTimes":"","isGiveGift":0,"giftType":1,
+        "openSell":"true","sellCloseDay":0,"isPriorityBuy":0,"priorityDate":"","priorityTimes":"","isGiveGift":0,"giftType":1,
         "showType":1,"giftId":"","distribution":1,"distributionProportion":"","holdPictures":[],"isOpen":0,"extraStock":33,
         "lowerShelfCooling":1,"openExclusiveBuy":0,"sellForbidden":0,"firstGetInterdiction":0,"marketGetInterdiction":0,
         "resellFixedPriceStatus":0,"fixedPriceBasis":0,"calculationFormula":0,"publishEndTime":"2023-08-24 00:00:00",
@@ -32,7 +36,7 @@ class TestRequest:
         "exclusiveRepertory":32,"limitBrush":1,"timesMode":1,"openDiscount":1,"discountMode":1,"nftRepertorySeparationVo":{},
         "levelId":0,"nftAttrs":[{"attrId":1,"attrValue":100,"attrInitValue":100,"attr_name":"精力值"}],
         "chainSeriesDTO":{"chainUnitDTOS":[],"templateImg":"","raceId":1},"limitRule":[],"limitType":0,"exchangeTimeOpen":0,
-        "prizeGiftList":[],"integral":[{"integralType":2,"isShow":'"false"',"name":"电池配置"},{"integralType":3,"isShow":'false',"name":"砖块配置"},
+        "prizeGiftList":[],"integral":[{"integralType":2,"isShow":'false',"name":"电池配置"},{"integralType":3,"isShow":'false',"name":"砖块配置"},
                                        {"integralType":4,"isShow":'false',"name":"龙精配置"},{"integralType":5,"isShow":'false',"name":"金蛋配置"},
                                        {"integralType":6,"isShow":'false',"name":"龙珠配置"},{"integralType":7,"isShow":'false',"name":"龙核配置"},
                                        {"integralType":8,"isShow":'false',"name":"龙鳞配置"},{"integralType":9,"isShow":'false',"name":"龙血配置"},
@@ -43,12 +47,13 @@ class TestRequest:
         "nftPlunderDisposition":{"durabilityLimit2":0,"durabilityLimit":0,"energy":0,"physicalPowerScore":0,"lifeScore":0},
         "nftAttrConfig":{"attrType":6,"nftAttrConfigCostBOS":[]},"lifeMode":1,"consumeMode":1}
 
-        headers = {'Accept': 'application/json, text/plain','Accept-Language': 'zh-CN,zh;q=0.9','Authorization': TestRequest.token}
+        headers = {'Accept': 'application/json, text/plain', 'Accept-Language': 'zh-CN,zh;q=0.9', 'Authorization': TestRequest.token}
 
-        response = requests.post(url=url,json=data,headers=headers)
+        response = requests.request("post", url=url, json=data, headers=headers)
         print(response.json())
         assert response.status_code == 0
 
+    # 编辑藏品
     def testcase_edit_Collections(self):
         url = "higher8pre.douxiangapp.com/dboxAdmin/v1/nft/10000148"
         data = {"id":"10000148","title":"编辑自动化测试","selfCode":"null","description":"编辑自动化测试",
@@ -100,7 +105,7 @@ class TestRequest:
                 "supplementaryType":1,"nftPowerConfig":{"gameInstructions":"0","maxPower":0,"initPower":0,"consume":0},
                 "nftPlunderDisposition":"null","consumeMode":1,"nftAttrConfig":"null","recommend":"null","ordinaryRepertory":"null",
                 "pictureRepertory":"null","exclusiveRepertory":"null"}
-        response = requests.put(url=url,json=data)
+        response = requests.request("put", url=url, json=data)
         print(response.json())
         assert response.status_code == 0
 
@@ -108,5 +113,5 @@ class TestRequest:
 
 
     if __name__ == "__main__":
-        pytest.main(['-vs'])
+        pytest.main()
 
