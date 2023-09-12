d79e817cf1d865a604fbfb19bd884dfe4893f953
fu.yang <1003107864@qq.com>
2023-09-12 15:03:09 +0800

    修改日志

--- common/log_util.py
+++ common/log_util.py
@@ -1,29 +1,34 @@
 import logging
 import os
+import time
 
 
 def log_info(message):
+    root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    log_path = os.path.join(root_path, "log")
+    # 定义日志位置和文件名
+    logname = os.path.join(log_path, "{}.log".format(time.strftime("%Y-%m-%d")))
     # 创建一个日志记录器
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger('log')
+    # 设置日志打印的级别
     logger.setLevel(logging.DEBUG)
-
-    # 日志文件路径
-    log_file_path = os.path.join('()log', 'test.log')
-
-    # 移除之前添加的处理器，如果存在的话
-    for handler in logger.handlers:
-        logger.removeHandler(handler)
-
-    # 创建一个处理器，将日志同时写入文件和输出到控制台
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-
-    # 定义日志格式
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-
-    # 将处理器添加到日志记录器
-    logger.addHandler(handler)
-
+    # 创建日志输入的格式
+    formater = logging.Formatter(
+        '[%(asctime)s][%(filename)s %(lineno)d][%(levelname)s]: %(message)s')
+    # 创建日志处理器，用来存放日志文件
+    filelogger = logging.FileHandler(logname, mode='a', encoding="UTF-8")
+    # 创建日志处理器，在控制台打印
+    console = logging.StreamHandler()
+    # 设置控制台打印日志界别
+    console.setLevel(logging.DEBUG)
+    # 文件存放日志级别
+    filelogger.setLevel(logging.DEBUG)
+    # 文件存放日志格式
+    filelogger.setFormatter(formater)
+    # 控制台打印日志格式
+    console.setFormatter(formater)
+    # 将日志输出渠道添加到日志收集器中
+    logger.addHandler(filelogger)
+    logger.addHandler(console)
     # 记录日志信息
     logger.info(message)
