18d5c2c232d9559938eb1d2806322fff233dedf7
fu.yang <1003107864@qq.com>
2023-09-19 12:27:02 +0800

    增加计数器count.py

--- common/count.py
+++ common/count.py
@@ -1,11 +1,11 @@
 from config.color import print_centered_ansi
 
-test_case_counter = 0
+counter = 0
 
 
 def count(data):
-    global test_case_counter
+    global counter
     separator = '>' * 20
-    test_case_counter += 1  # 递增类级别的测试用例计数器
-    print_centered_ansi(f"\n{separator}这是第{test_case_counter}条用例{separator}\n", '33')
+    counter += 1  # 递增类级别的测试用例计数器
+    print_centered_ansi(f"\n{separator}这是第{counter}条用例{separator}\n", '33')
     return data
