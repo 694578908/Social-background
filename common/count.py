a6faada01c30f125102d49239306d7a0a07ce12d
fu.yang <1003107864@qq.com>
2023-09-25 15:34:19 +0800

    修改count

--- common/count.py
+++ common/count.py
@@ -7,5 +7,5 @@ def count(data):
     global counter
     separator = '>' * 20
     counter += 1  # 递增类级别的测试用例计数器
-    print_centered_ansi(f"\n{separator}这是第{counter}条用例{separator}\n", '33')
+    print_centered_ansi(f"\n{separator}执行第{counter}条用例{separator}\n", '33')
     return data
